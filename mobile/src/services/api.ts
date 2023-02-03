35b66a00a65980525f29ee4e2e6c514000229432
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-02-02 22:17:16 -0300

    Entendendo Interceptors

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -3,3 +3,10 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'http://192.168.1.5:3333',
 });
+
+api.interceptors.response.use(response => {
+  return response;
+}, error => {
+  console.log('interceptor =>', error);
+  return Promise.reject(error);
+});

be2bcb1897b04f264548cb6acf573cea2460aeed
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-02-02 22:00:11 -0300

    Axios Exceptions

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -3,11 +3,13 @@ import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import axios from 'axios'
 import { api } from '@services/api'
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import BackgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
+import { Alert } from 'react-native';
 
 type FormDataProps = {
   name: string;
@@ -42,13 +44,19 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post('/users', {
-      name,
-      email,
-      password,
-    });
+    try {
+      const response = await api.post('/users', {
+        name,
+        email,
+        password,
+      });
 
-    console.log(response.data);
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   }
 
   return (

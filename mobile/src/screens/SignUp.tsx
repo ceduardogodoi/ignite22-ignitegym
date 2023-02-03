ace42831599e83419d5601a38e7f1967c40bc249
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-02-02 21:49:35 -0300

    Utilizando Axios

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -3,6 +3,7 @@ import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { api } from '@services/api'
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 import BackgroundImg from '@assets/background.png';
@@ -41,21 +42,13 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch('http://192.168.1.11:3333/users', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password,
-      })
+    const response = await api.post('/users', {
+      name,
+      email,
+      password,
     });
 
-    const data = await response.json();
-    console.log(data);
+    console.log(response.data);
   }
 
   return (

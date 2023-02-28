0378d6fa764d6fe1c61b8902798f1ef58d14a551
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-02-27 21:52:39 -0300

    Exibindo nome do usuário

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -1,9 +1,12 @@
 import { TouchableOpacity } from 'react-native';
 import { Heading, HStack, Text, VStack, Icon } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
+import { useAuth } from '@hooks/useAuth';
 import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
@@ -15,7 +18,7 @@ export function HomeHeader() {
 
       <VStack flex={1}>
         <Text color="gray.100" fontSize="md">Olá, </Text>
-        <Heading color="gray.100" fontSize="md" fontFamily="heading">Rodrigo</Heading>
+        <Heading color="gray.100" fontSize="md" fontFamily="heading">{user.name}</Heading>
       </VStack>
 
       <TouchableOpacity>

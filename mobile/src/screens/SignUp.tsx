40053a78cd58879069828e08a145e77899bbdd2c
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-02-27 22:08:59 -0300

    Criando conta e logando usuário

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { Center, Heading, Image, Text, VStack, ScrollView, useToast } from 'native-base';
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useAuth } from '@hooks/useAuth';
 import { api } from '@services/api'
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
@@ -32,7 +34,10 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const toast = useToast();
+  const { signIn } = useAuth();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -45,15 +50,19 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
+    setIsLoading(true);
+
     try {
-      const response = await api.post('/users', {
+      await api.post('/users', {
         name,
         email,
         password,
       });
 
-      console.log(response.data);
+      await signIn(email, password);
     } catch (error) {
+      setIsLoading(false);
+
       const isAppError = error instanceof AppError;
       const title = isAppError ?
         error.message :
@@ -167,6 +176,7 @@ export function SignUp() {
           <Button
             title="Criar e acessar"
             onPress={handleSubmit(handleSignUp)}
+            isLoading={isLoading}
           />
         </Center>
 

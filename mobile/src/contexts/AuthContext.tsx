34446a8e9ac94c443dde237f41900f4168aae70f
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-03-06 21:45:35 -0300

    Obtendo Token do usuário

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -22,7 +22,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   async function signIn(email: string, password: string) {
     const { data } = await api.post('/sessions', { email, password });
-    if (data.user) {
+    if (data.user && data.token) {
       setUser(data.user);
       storageUserSave(data.user);
     }

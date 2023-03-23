9a6355845568a34ec2507fdfa8cfd3ce861448ed
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-03-23 08:55:10 -0300

    Reenviando a requisição

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -58,6 +58,19 @@ api.registerInterceptTokenManager = signOut => {
             });
 
             await storageAuthTokenSave({ token: data.token, refresh_token: data.refresh_token });
+
+            if (originalRequestConfig.data) {
+              originalRequestConfig.data = JSON.parse(originalRequestConfig.data);
+            }
+
+            originalRequestConfig.headers = { Authorization: `Bearer ${data.token}` };
+            api.defaults.headers.common.Authorization = `Bearer ${data.token}`;
+
+            failedQueue.forEach(request => {
+              request.onSuccess(data.token);
+            });
+
+            resolve(api(originalRequestConfig));
           } catch (error: any) {
             failedQueue.forEach(request => {
               request.onFailure(error);
